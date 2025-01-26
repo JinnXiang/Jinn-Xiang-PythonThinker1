900ba2a88ee424073de128a25edf47d75fbdbe62
JinnXiang <Ljinnxiang@gmail.com>
2025-01-26 03:40:10 +0000

    Jan 26, 2025, 3:40 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,11 @@
-print("Hello from lesson 3")
+#print("Hello from lesson 3")
+#Give Adam a score of 60
+#Give Brandon a score of 58
+#Give Charlie a score of 98
+#Give Daniel a score of 79
+#Add all the scores of Adam, Brandon, Charlie and Daniel together
+#Divide current number by the number of students(4)
+#Print out the number in the end
+
+
+A

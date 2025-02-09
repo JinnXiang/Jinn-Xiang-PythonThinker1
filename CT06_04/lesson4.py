6117da8e9e1c92253690a29a9420efb6c433e1c0
JinnXiang <Ljinnxiang@gmail.com>
2025-02-09 03:55:32 +0000

    Feb 9, 2025, 3:55 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,3 +1,6 @@
-name=input("what is your name")
-print ("Hi, "+name)
+name=input("what is your name?")
+print ("Hi, " + name + ".")
 hobby=input("what is your hobby?")
+print ("I enjoy " + hobby + ".")
+vacation=input("what is your dream vacation destination?")
+print ("I would love to visit " + vacation + ".")

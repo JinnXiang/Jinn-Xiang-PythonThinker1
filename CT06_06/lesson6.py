7e27516f6d7f4a801d95a249a39e615b65a1d666
JinnXiang <Ljinnxiang@gmail.com>
2025-02-23 03:48:44 +0000

    Feb 23, 2025, 3:48 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -22,3 +22,22 @@
 # print(x)
 
 # print("Hello, World!")
+
+age = "25"
+print(age + 1)
+
+number = 10
+print(number - "5")
+
+print("Repeat" * "3")
+
+year = 2023
+print("The year is " + year)
+
+x = "10"
+y = x / 2
+
+**Task 3f**:
+end = "5"
+for i in range(end):
+    print(i)

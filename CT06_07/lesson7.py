55e2d0f28d022a55b70e7d5971898bcdfd80fafb
JinnXiang <Ljinnxiang@gmail.com>
2025-03-02 03:56:48 +0000

    Mar 2, 2025, 3:56 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -47,5 +47,5 @@
 #     print("Nice to meet you, "+ n)
 
 for n in range(1,6):
-    input("What is number #"+n+"?")
+    input("What is number #" +"?")
 print("The sum of the numbers you picked is "+str(total))

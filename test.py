53aea78fd36bfa8549a8b98ef86bd2dc95bfc838
JinnXiang <Ljinnxiang@gmail.com>
2025-03-09 03:42:24 +0000

    Mar 9, 2025, 3:42 AM

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ start=int(input("what is the start number?"))#find the start number
 end=int(input("what is the end number?"))#find the end number
 increment=int(input("what is the increment?"))#find the increment
 for k in range(start,end,increment):#do the range function
-    print(k)#print
+    print(k)#print the answer

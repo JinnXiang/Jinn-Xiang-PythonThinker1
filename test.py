667ffea603856e693a0ab26e0d24bd09443f4887
JinnXiang <Ljinnxiang@gmail.com>
2025-03-09 03:48:57 +0000

    Mar 9, 2025, 3:48 AM

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 # name=input("What is your name")#ask the user what is their name
 # print("Nice to meet you, "+name)#print out the greeting and the user's name
 
-start=int(input("what is the start number?"))#find the start number
-end=int(input("what is the end number?"))#find the end number
-increment=int(input("what is the increment?"))#find the increment
-for k in range(start,end,increment):#do the range function
-    print(k)#print the answer
+# start=int(input("what is the start number?"))#find the start number
+# end=int(input("what is the end number?"))#find the end number
+# increment=int(input("what is the increment?"))#find the increment
+# for k in range(start,end,increment):#do the range function
+#     print(k)#print the answer
+
+name = "DENNIS"
+for letter in range(10):
+	print(letter)

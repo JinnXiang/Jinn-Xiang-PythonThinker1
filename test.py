c24f6b4a02072682b4e24e3bc496b500bd897e23
JinnXiang <Ljinnxiang@gmail.com>
2025-03-09 03:38:49 +0000

    Mar 9, 2025, 3:38 AM

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 # name=input("What is your name")#ask the user what is their name
 # print("Nice to meet you, "+name)#print out the greeting and the user's name
 
-start=input("what is the start number?")
+start=int(input("what is the start number?"))
 end=input("what is the end number?")
 increment=input("what is the increment?")
-for k in range(int(start,end,increment)):
+for k in range(start,end,increment):
     print(k)

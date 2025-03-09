3f76e6bbf45152c6883267c0ec7b87da493df7ed
JinnXiang <Ljinnxiang@gmail.com>
2025-03-09 03:33:09 +0000

    Mar 9, 2025, 3:33 AM

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-name=input("What is your name")#ask the user what is their name
-print("Nice to meet you, "+name)#print out the greeting and the user's name
+# name=input("What is your name")#ask the user what is their name
+# print("Nice to meet you, "+name)#print out the greeting and the user's name

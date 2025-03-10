2beaab55ccaf0d4759903b92da5250552e967ba4
JinnXiang <Ljinnxiang@gmail.com>
2025-03-10 09:58:08 +0000

    Mar 10, 2025, 9:58 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,5 +1,13 @@
 # print("Hello from lesson 8")
 
-for h in range(5):
-    num=input("Tell me a number")
-print(int(num)*5)
+# for h in range(5):
+#     num=input("Tell me a number")
+# print(int(num)*5)
+
+
+for i in range(5):
+    num = float(input(f"Enter number {i+1}: "))
+    result *= num  # Multiply the result by the entered number
+
+# Print the final multiplication result
+print(f"The multiplication of the entered numbers is: {result}")

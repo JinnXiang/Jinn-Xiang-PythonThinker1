94d1d371a8e81641088656a3da6fa0f65b64081e
JinnXiang <Ljinnxiang@gmail.com>
2025-03-10 09:32:34 +0000

    Mar 10, 2025, 9:32 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -8,14 +8,14 @@
 
 
 
-age=input("what is your age")
-age=int(age) + 1
-print ("Next year, you will be " + str(age) + " years old.")
+# age=input("what is your age")
+# age=int(age) + 1
+# print ("Next year, you will be " + str(age) + " years old.")
 
-num=input("write down a number")
-num=int(num)*2
-print ("Double your number is " + str(num))
+# num=input("write down a number")
+# num=int(num)*2
+# print ("Double your number is " + str(num))
 
-born=input("when were you born")
-born=20252025202520252025202520252025202520252025-int(born)
-print ("you are "+ str(born)+" years old")
+# born=input("when were you born")
+# born=20252025202520252025202520252025202520252025-int(born)
+# print ("you are "+ str(born)+" years old")

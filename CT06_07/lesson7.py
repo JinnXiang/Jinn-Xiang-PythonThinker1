bd0b236120013dc63d14958b1a2bf2e0654e72f0
JinnXiang <Ljinnxiang@gmail.com>
2025-03-10 09:31:59 +0000

    Mar 10, 2025, 9:31 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -66,6 +66,6 @@
 # for h in range(1,num+1):
 #     print(str(h)*h)
 
-number=int(input("tell me a number"))
-for n in range(1,number+1):
-    print(str(n)*n)
+# number=int(input("tell me a number"))
+# for n in range(1,number+1):
+#     print(str(n)*n)

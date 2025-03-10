c625340c0fb6cf9ce4442a0677e8f7469a802c09
JinnXiang <Ljinnxiang@gmail.com>
2025-03-10 10:02:59 +0000

    Mar 10, 2025, 10:02 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -4,3 +4,12 @@
 #     num=input("Tell me a number")
 # print(int(num)*5)
 
+result = 1
+
+# Use a for loop to ask for 5 numbers
+for i in range(5):
+    num = float(input("Enter number {}: ".format(i+1)))  # Input for each number
+    result *= num  # Multiply the result by the entered number
+
+# Print the final multiplication result
+print("The multiplication of the entered numbers is: {}".format(result))

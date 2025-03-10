13c29a5a4248683191203cce20f0cd74007bff25
JinnXiang <Ljinnxiang@gmail.com>
2025-03-10 10:07:57 +0000

    Mar 10, 2025, 10:07 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -2,9 +2,9 @@
 
 import time
 
-for i in range(10000000,0,-1):
+for i in range(10,0,-1):
     print(i)
-    time.sleep(0.000000000000000000000000000000000000000000000000000000000001)
+    time.sleep(1)
 
 print("Go!")
 

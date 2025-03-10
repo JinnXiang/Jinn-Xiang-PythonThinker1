bcfe968cf59f4ce119889b85f5cfb57b40dfc85d
JinnXiang <Ljinnxiang@gmail.com>
2025-03-10 10:14:09 +0000

    Mar 10, 2025, 10:14 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -8,13 +8,13 @@
 
 # print("Go!")
 
-num=input("Tell me a number")
-import time
+# num=input("Tell me a number")
+# import time
 
-for i in range(int(num),0,-1):
-    print(i)
-    time.sleep(1)
+# for i in range(int(num),0,-1):
+#     print(i)
+#     time.sleep(1)
 
-print("Go!")
+# print("Go!")
 
 

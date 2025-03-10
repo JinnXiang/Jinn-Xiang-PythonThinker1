31c0ec787a8173689764c439c938f57a2f4031ed
JinnXiang <Ljinnxiang@gmail.com>
2025-03-10 10:49:25 +0000

    Mar 10, 2025, 10:49 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -39,8 +39,8 @@
 import random
 num1 = random.randint (1,50)
 num2 = random.randint (1,50)
-question= "What is " + str(num1) + "+" + str(num2)  +"?"
-reply=input(question)
+question= "What is " + str(num1) + "+" + str(num2) + "?"
+reply = input (question)
 reply=int(reply)
 hidden + num1 + num2
 print (reply == hidden )

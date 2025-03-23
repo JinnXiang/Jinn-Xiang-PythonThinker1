5014bde2fbc0b0820aa3159bf883bd57fdea2179
JinnXiang <Ljinnxiang@gmail.com>
2025-03-23 03:55:31 +0000

    Mar 23, 2025, 3:55 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,18 +1,19 @@
-print("Hello from lesson 9")
+# print("Hello from lesson 9")
 
-import random
+# import random
 
-num1 = random.randint(1,9999999999999999999)
-num2 = random.randint(1,9999999999999999999)
-num3 = random.randint(1,9999999999999999999)
+# num1 = random.randint(1,9999999999999999999)
+# num2 = random.randint(1,9999999999999999999)
+# num3 = random.randint(1,9999999999999999999)
 
-print("1st number: " + str(num1))
-print("2nd number: " + str(num2))
-print("3rd number: " + str(num3))
+# print("1st number: " + str(num1))
+# print("2nd number: " + str(num2))
+# print("3rd number: " + str(num3))
 
-num1_even = num1 % 2 == 0
-num2_even = num2 % 2 == 0
-num3_even = num3 % 2 == 0
+# num1_even = num1 % 2 == 0
+# num2_even = num2 % 2 == 0
+# num3_even = num3 % 2 == 0
 
-all_even_odd = num1_even == num2_even == num3_even
-print("All numbers are even/odd is " + str(all_even_odd))
+# all_even_odd = num1_even == num2_even == num3_even
+# print("All numbers are even/odd is " + str(all_even_odd))
+

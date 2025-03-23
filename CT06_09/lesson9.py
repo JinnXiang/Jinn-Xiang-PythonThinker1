4342ec21bdba0ce21fae45e440396c8b448e3378
JinnXiang <Ljinnxiang@gmail.com>
2025-03-23 04:27:13 +0000

    Mar 23, 2025, 4:27 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -42,7 +42,7 @@ if numOrange <= 5:
 
 
 if numApple > 5:
-    costApple=costApple+numApple*px_apple*0.6
+    costApple=costApple+numApple*px_apple*0.9
 
 if numApple<=5:
     cpstApple = costApple + numApple*px_apple

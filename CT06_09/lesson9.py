b387b3f7d3c90f6ad00f966c12bcc5d4c4181887
JinnXiang <Ljinnxiang@gmail.com>
2025-03-23 03:29:18 +0000

    Mar 23, 2025, 3:29 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -3,4 +3,5 @@ print("Hello from lesson 9")
 num1 = int(input("What is the first number?"))
 num2 = int(input("What is the second number?"))
 num3 = int(input("What is the third number?"))
-im
+import random
+

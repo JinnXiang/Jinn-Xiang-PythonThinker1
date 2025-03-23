e21b4eed7a3a8ab1e9b0c2a768798649930c4d25
JinnXiang <Ljinnxiang@gmail.com>
2025-03-23 03:54:16 +0000

    Mar 23, 2025, 3:54 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -2,8 +2,8 @@ print("Hello from lesson 9")
 
 import random
 
-num1 = random.randint(1,6)
-num2 = random.randint(1,6)
+num1 = random.randint(1,9999999999999999999)
+num2 = random.randint(1,9999999999999999999)
 num3 = random.randint(1,6)
 
 print("1st number: " + str(num1))

b8192e39263bc190999743121044147a31fd6a67
JinnXiang <Ljinnxiang@gmail.com>
2025-03-23 03:30:07 +0000

    Mar 23, 2025, 3:30 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -2,7 +2,7 @@ print("Hello from lesson 9")
 
 import random
 
-num1 = int(input("What is the first number?"))
+num1 = random.randint()
 num2 = int(input("What is the second number?"))
 num3 = int(input("What is the third number?"))
 

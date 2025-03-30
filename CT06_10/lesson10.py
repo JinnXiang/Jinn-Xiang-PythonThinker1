d016b8e85ec7328d6240b6eeab5618f84f57ab91
JinnXiang <Ljinnxiang@gmail.com>
2025-03-30 04:22:33 +0000

    Mar 30, 2025, 4:22 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -28,6 +28,6 @@ elif score > 79:
 elif score > 69:
     print("Can you kick youself out the door?")
 elif score > 59:
-    print("Dis")
+    print("Disappear")
 else:
     print("Failure to thrive")

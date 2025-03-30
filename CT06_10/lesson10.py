9acc1f33a8760e0dcef76a2da4d943f2f6c90e1f
JinnXiang <Ljinnxiang@gmail.com>
2025-03-30 03:56:15 +0000

    Mar 30, 2025, 3:56 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -2,6 +2,6 @@ print("Hello from lesson 10")
 
 num=int(input("Give me a number:"))
 if num>0:
-    print(num +"is positive!")
+    print(num + ("is positive!"))
 else:
-    print(num +"is not positive!")
+    print(num + "is not positive!")

cb1342c2a264f9da6e3abebccd103611b14fe073
JinnXiang <Ljinnxiang@gmail.com>
2025-04-06 03:13:52 +0000

    Apr 6, 2025, 3:13 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -4,7 +4,7 @@ px = int(input("What is the price of the item?"))
 if px <= 5:
     print("Too cheap!")
 elif px <= 50:
-    print("Just spend more money.")
+    print("Are you this poor?")
 elif px <= 500:
     print("About right...Just a little more")
 else:

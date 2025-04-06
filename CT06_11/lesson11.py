087234163725f2088e7929262cb60a7a0f2319ec
JinnXiang <Ljinnxiang@gmail.com>
2025-04-06 03:12:27 +0000

    Apr 6, 2025, 3:12 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -4,7 +4,7 @@ px = int(input("What is the price of the item?"))
 if px <= 5:
     print("Too cheap!")
 elif px <= 50:
-    print("Are you sure you need this?")
+    print("")
 elif px <= 500:
     print("Where are you getting this money from?!")
 else:

8fca837f9b195538f323fdd4ce2fbddd042b9d6a
JinnXiang <Ljinnxiang@gmail.com>
2025-04-06 04:24:56 +0000

    Apr 6, 2025, 4:24 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -67,7 +67,7 @@
 #     print("Access denied")
 
 
-# game_status = "ac
+# game_status = input
 # if game_status == "active" or not game_status == "paused":
 #     print("Game in progress")
 # else:

4368ab9222e0d7aeb2b4d19c1003b64d013c3bfd
JinnXiang <Ljinnxiang@gmail.com>
2025-04-06 04:17:49 +0000

    Apr 6, 2025, 4:17 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -58,7 +58,7 @@
 username = "John123"
 pw = "pw123"
 enter_username = input("What is your username?")
-enter
+enter_pw = input
 
 # game_status = "active"
 # if game_status == "active" or not game_status == "paused":

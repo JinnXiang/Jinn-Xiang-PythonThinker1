0049330d7fe76a8b0c93bc0be4c0894e1237f219
JinnXiang <Ljinnxiang@gmail.com>
2025-04-13 04:12:26 +0000

    Apr 13, 2025, 4:12 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -44,7 +44,7 @@ while True:
             order += item
             Skipcomma = False
         else:
-            order = " ," + item + order
+            order = ", " + item + order
 
 print(order)
         

fe4e36bd832e6d96ff0d9f77c953241fba875548
JinnXiang <Ljinnxiang@gmail.com>
2025-04-13 03:12:19 +0000

    Apr 13, 2025, 3:12 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,1 +1,7 @@
-print("Hello from lesson 12")
+# print("Hello from lesson 12")
+
+# num=int(input("What is the number?"))
+# if num % 3 == 0 and num % 5 == 0:
+#     print("Divisible by 3 and 5")
+# else:
+#     print("Not divisible by 3 and 5")

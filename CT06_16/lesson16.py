5bb6b3efd5f9a548ae03b511a2d27e6ccc05ced6
JinnXiang <Ljinnxiang@gmail.com>
2025-05-18 03:41:38 +0000

    May 18, 2025, 3:41 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -7,12 +7,13 @@ def setup_screen (screenWidth, screenHeight):
     screen.setup(width = screenWidth, height = screenHeight)
     return screen
 
-window = turtle.Screen()
-window.setup(width=300, height=500)
-t = turtle.Turtle()
-t.shape("turtle")
-t.fillcolor("black")
-t.pendown()
+
+
+
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.fillcolor("black")
+# t.pendown()
 
 
 

f4cfd53550e68b33fb94f5788213cc071bcdc8a7
JinnXiang <Ljinnxiang@gmail.com>
2025-05-18 03:34:51 +0000

    May 18, 2025, 3:34 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -14,12 +14,9 @@ t.pendown()
 #     t.forward(250)
 #     t.left(144)
 #     t.left(5)
-
-
-
-def drawShape(length,num_sides):
-    for i in range(num_sides):
-        t.forward(length)
-        t.right(360/num_sides)
-drawShape(100,50)
-window.mainloop()
+# def drawShape(length,num_sides):
+#     for i in range(num_sides):
+#         t.forward(length)
+#         t.right(360/num_sides)
+# drawShape(100,50)
+# window.mainloop()

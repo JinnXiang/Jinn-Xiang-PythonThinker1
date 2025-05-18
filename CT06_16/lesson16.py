f82ab21796cbd8f4276e2330f2f1b07bae67b6e3
JinnXiang <Ljinnxiang@gmail.com>
2025-05-18 04:33:39 +0000

    May 18, 2025, 4:33 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,47 +1,47 @@
 # print("Hello from lesson 16")
 
-import turtle
+# import turtle
 
-def setup_screen (screenWidth, screenHeight):
-    screen = turtle.Screen()
-    screen.setup(width = screenWidth, height = screenHeight)
-    return screen
+# def setup_screen (screenWidth, screenHeight):
+#     screen = turtle.Screen()
+#     screen.setup(width = screenWidth, height = screenHeight)
+#     return screen
 
-def create_blue_ball():
-    ball = turtle.Turtle()
-    ball.shape("circle")
-    ball.color("blue")
-    ball.penup()
-    return ball
+# def create_blue_ball():
+#     ball = turtle.Turtle()
+#     ball.shape("circle")
+#     ball.color("blue")
+#     ball.penup()
+#     return ball
 
-def move_ball(ball, dx, dy):
-    ball.setx(ball.xcor() + dx)
-    ball.sety(ball.ycor() + dy)
+# def move_ball(ball, dx, dy):
+#     ball.setx(ball.xcor() + dx)
+#     ball.sety(ball.ycor() + dy)
 
-def check_x(ball, screenWidth):
-    if ball.xcor() > (screenWidth / 2) or ball.xcor() < (-screenWidth / 2):
-        return True
+# def check_x(ball, screenWidth):
+#     if ball.xcor() > (screenWidth / 2) or ball.xcor() < (-screenWidth / 2):
+#         return True
 
-def check_y(ball, screenHeight):
-    if ball.ycor() > (screenHeight / 2) or ball.ycor() < (-screenHeight / 2):
-        return True
+# def check_y(ball, screenHeight):
+#     if ball.ycor() > (screenHeight / 2) or ball.ycor() < (-screenHeight / 2):
+#         return True
 
 
-screenWidth = 300
-screenHeight = 500
-screen = setup_screen(screenWidth, screenHeight)
-ball = create_blue_ball()
+# screenWidth = 300
+# screenHeight = 500
+# screen = setup_screen(screenWidth, screenHeight)
+# ball = create_blue_ball()
 
-dx = 2
-dy = 2
-while True:
-    move_ball(ball, dx, dy)
-    if check_x(ball, screenWidth):
-        dx *= -1
-    if check_y(ball, screenHeight):
-        dy *= -1
+# dx = 2
+# dy = 2
+# while True:
+#     move_ball(ball, dx, dy)
+#     if check_x(ball, screenWidth):
+#         dx *= -1
+#     if check_y(ball, screenHeight):
+#         dy *= -1
 
-screen.mainloop()
+# screen.mainloop()
 
 
 
